4d740fbf6741f41f3bb5435a0ba99610e6736f28
Yaroslav <yaroslav-w@mail.ru>
2022-09-06 23:55:18 +0300

    feat: Добавлена модель пользователя для СУБД.

---
+++ src/main/java/org/yaroslav/infrastructure/models/UserModel.java
@@ -0,0 +1,77 @@
+package org.yaroslav.infrastructure.models;
+
+import javax.persistence.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.hibernate.validator.constraints.UniqueElements;
+import org.yaroslav.domain.entities.User;
+
+/**
+ * Модель пользователя для СУБД.
+ */
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+public class UserModel {
+	/**
+	 * Идентификатор пользователя.
+	 */
+	@Id
+	@GeneratedValue
+	@Column(name = "id", unique = true)
+	@UniqueElements
+	private @Getter long userId;
+
+	/**
+	 * Имя пользователя.
+	 */
+	@Column(nullable = false)
+	private @Getter String name;
+
+	/**
+	 * Электронная почта пользователя.
+	 */
+	@Column(unique = true, nullable = false)
+	private @Getter String email;
+
+	/**
+	 * Пароль пользователя.
+	 */
+	@Column(nullable = false)
+	private @Getter String password;
+
+	/**
+	 * Изображение пользователя.
+	 */
+	@Column(nullable = false)
+	private @Getter String image;
+
+	/**
+	 * Роль пользователя, см. {@link User.Role}.
+	 */
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private @Getter User.Role role;
+
+	protected UserModel() {}
+
+	/**
+	 * Создаёт экземпляр класса {@link UserModel}.
+	 *
+	 * @param name Имя пользователя.
+	 * @param email Электронная почта пользователя.
+	 * @param password Пароль пользователя.
+	 * @param image Изображение пользователя.
+	 * @param role Роль пользователя.
+	 */
+	public UserModel(
+			String name,
+			String email,
+			String password,
+			String image,
+			User.Role role
+	) {
+		this(0, name, email, password, image, role);
+	}
+}
