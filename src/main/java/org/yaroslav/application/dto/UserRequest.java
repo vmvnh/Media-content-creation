95ee9825a8639ac5c9ff9b745c08c7d3894f9286
Yaroslav <yaroslav-w@mail.ru>
2022-09-30 02:16:47 +0400

    feat: Добавлено представление сущности пользователя (запрос).

---
+++ src/main/java/org/yaroslav/application/dto/UserRequest.java
@@ -0,0 +1,49 @@
+package org.yaroslav.application.dto;
+
+import org.yaroslav.domain.entities.User;
+
+/**
+ * Представление сущности пользователя (запрос).
+ */
+public class UserRequest {
+
+	/**
+	 * Имя пользователя.
+	 */
+	public String name;
+
+	/**
+	 * Электронная почта пользователя.
+	 */
+	public String email;
+
+	/**
+	 * Пароль пользователя.
+	 */
+	public String password;
+
+	/**
+	 * Изображение пользователя.
+	 */
+	public String image;
+
+	/**
+	 * Роль пользователя, см. {@link User.Role}.
+	 */
+	public User.Role role;
+
+	private UserRequest() {}
+
+	/**
+	 * Создаёт экземпляр класса {@link UserRequest}.
+	 *
+	 * @param user Сущность пользователя.
+	 */
+	public UserRequest(User user) {
+		this.name = user.name();
+		this.email = user.email();
+		this.password = user.password();
+		this.image = user.image();
+		this.role = user.role();
+	}
+}
