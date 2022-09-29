6983eb798c6751ad66eba932cd77c3c921cd4201
Yaroslav <yaroslav-w@mail.ru>
2022-09-30 02:22:14 +0400

    feat: Добавлен конфигуратор сценариев использования пользователей.

---
+++ src/main/java/org/yaroslav/config/UserBeansConfig.java
@@ -0,0 +1,17 @@
+package org.yaroslav.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.yaroslav.domain.repositories.UserRepository;
+import org.yaroslav.domain.usecase.UserCreate;
+
+/**
+ * Конфигуратор сценариев использования пользователей.
+ */
+@Configuration
+public class UserBeansConfig {
+	@Bean
+	public UserCreate create(UserRepository repository) {
+		return new UserCreate(repository);
+	}
+}
