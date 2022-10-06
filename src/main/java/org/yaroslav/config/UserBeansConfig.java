c6792be665fd30abbeb10ba39a4f8423631c2484
Yaroslav <yaroslav-w@mail.ru>
2022-10-07 03:57:05 +0400

    feat: Добавлена конфигурация бина для сценария получения всех пользователей.

--- src/main/java/org/yaroslav/config/UserBeansConfig.java
+++ src/main/java/org/yaroslav/config/UserBeansConfig.java
@@ -3,6 +3,7 @@ package org.yaroslav.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.yaroslav.domain.repositories.UserRepository;
+import org.yaroslav.domain.usecase.GetAllUsers;
 import org.yaroslav.domain.usecase.UserCreate;
 
 /**
@@ -13,5 +14,10 @@ public class UserBeansConfig {
 	@Bean
 	public UserCreate create(UserRepository repository) {
 		return new UserCreate(repository);
+	}
+
+	@Bean
+	public GetAllUsers getAllUsers(UserRepository repository) {
+		return new GetAllUsers(repository);
 	}
 }
