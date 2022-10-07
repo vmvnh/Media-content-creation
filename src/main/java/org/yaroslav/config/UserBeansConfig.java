9885c70ec471363db5d410fed0e1e391d22b2d3e
Yaroslav <yaroslav-w@mail.ru>
2022-10-07 21:44:14 +0400

    fix: Добавлен бин для сценария получения пользователя по идентификатору.

--- src/main/java/org/yaroslav/config/UserBeansConfig.java
+++ src/main/java/org/yaroslav/config/UserBeansConfig.java
@@ -3,8 +3,9 @@ package org.yaroslav.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.yaroslav.domain.repositories.UserRepository;
+import org.yaroslav.domain.usecase.CreateUser;
 import org.yaroslav.domain.usecase.GetAllUsers;
-import org.yaroslav.domain.usecase.UserCreate;
+import org.yaroslav.domain.usecase.GetUserById;
 
 /**
  * Конфигуратор сценариев использования пользователей.
@@ -12,12 +13,17 @@ import org.yaroslav.domain.usecase.UserCreate;
 @Configuration
 public class UserBeansConfig {
 	@Bean
-	public UserCreate create(UserRepository repository) {
-		return new UserCreate(repository);
+	public CreateUser create(UserRepository repository) {
+		return new CreateUser(repository);
 	}
 
 	@Bean
 	public GetAllUsers getAllUsers(UserRepository repository) {
 		return new GetAllUsers(repository);
+	}
+
+	@Bean
+	public GetUserById getUserById(UserRepository repository) {
+		return new GetUserById(repository);
 	}
 }
