123b878c5e21fdb51056f2542337a2a22c3f5f89
Yaroslav <yaroslav-w@mail.ru>
2022-10-10 19:38:33 +0400

    feat: Добавлены бины для сценариев пользователя: получение по email, обновление пользователя по идентификатору, удаление пользователя по идентификатору.

--- src/main/java/org/yaroslav/config/UserBeansConfig.java
+++ src/main/java/org/yaroslav/config/UserBeansConfig.java
@@ -4,8 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.yaroslav.domain.repositories.UserRepository;
 import org.yaroslav.domain.usecase.CreateUser;
+import org.yaroslav.domain.usecase.DeleteUserById;
 import org.yaroslav.domain.usecase.GetAllUsers;
 import org.yaroslav.domain.usecase.GetUserById;
+import org.yaroslav.domain.usecase.GetUserByEmail;
+import org.yaroslav.domain.usecase.UpdateUserById;
 
 /**
  * Конфигуратор сценариев использования пользователей.
@@ -25,5 +28,20 @@ public class UserBeansConfig {
 	@Bean
 	public GetUserById getUserById(UserRepository repository) {
 		return new GetUserById(repository);
+	}
+
+	@Bean
+	public GetUserByEmail getUserByEmail(UserRepository repository) {
+		return new GetUserByEmail(repository);
+	}
+
+	@Bean
+	public UpdateUserById updateUserById(UserRepository repository) {
+		return new UpdateUserById(repository);
+	}
+
+	@Bean
+	public DeleteUserById deleteUserById(UserRepository repository) {
+		return new DeleteUserById(repository);
 	}
 }
