0775115cc5969fd3c6198051f17038b403e66a17
Yaroslav <yaroslav-w@mail.ru>
2022-10-12 02:54:32 +0400

    feat: Добавлена проверка существования пользователя по email перед созданием.

--- src/main/java/org/yaroslav/domain/usecase/CreateUser.java
+++ src/main/java/org/yaroslav/domain/usecase/CreateUser.java
@@ -8,9 +8,13 @@ import org.yaroslav.domain.repositories.UserRepository;
  * Сценарий создания пользователя.
  */
 @AllArgsConstructor
-public class UserCreate {
+public class CreateUser {
 	private UserRepository repository;
+
 	public void createUser(String name, String email, String password, String image, User.Role role) {
-		repository.create(name, email, password, image, role);
+		var userExists = repository.get(email);
+		if (userExists.isEmpty()) {
+			repository.create(name, email, password, image, role);
+		}
 	}
 }
