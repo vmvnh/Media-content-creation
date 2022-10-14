bdeedf58d10675a2d383db34f056244ee025cf16
Yaroslav <yaroslav-w@mail.ru>
2022-10-14 23:58:58 +0400

    feat: Добавлен сценарий обновления пользователя.

---
+++ src/main/java/org/yaroslav/domain/usecase/UpdateUserById.java
@@ -0,0 +1,26 @@
+package org.yaroslav.domain.usecase;
+
+import lombok.AllArgsConstructor;
+import org.yaroslav.domain.entities.User;
+import org.yaroslav.domain.repositories.UserRepository;
+
+/**
+ * Сценарий обновления пользователя.
+ */
+@AllArgsConstructor
+public class UpdateUserById {
+  private UserRepository repository;
+  public void updateUser(
+      long id,
+      String name,
+      String email,
+      String password,
+      String image,
+      User.Role role
+  ) {
+    var userExists = repository.get(id);
+    if (userExists.) {
+      repository.update(name, email, password, image, role);
+    }
+  }
+}
